2f6f2ae7ba695ddc15c75978b238f1b6c9b82f8c
joel1723 <75437050+joel1723@users.noreply.github.com>
2022-01-08 12:52:32 -0500

    robot attack

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,25 @@
 
-function fight() {
-    window.alert("the fight has begun!")
-}
+
+window.alert("the fight has begun!")
+
+var playerName = window.prompt("what is your robot's name ?")
+var playerHealth = 100;
+var playerAttack = 10;
+
+var enemyName = "roberto"
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function () {
+    window.alert("welcome to robot gladiators");
+    playerAttack - enemyHealth
+    console.log(playerAttack - enemyHealth)
+    enemyAttack - playerHealth
+    console.log(enemyAttack - playerHealth)
+    console.log(playerName + "  attacked " + enemyName) + " . " + enemyName + " now has " + enemyHealth + "  health remaining "
+
+};
+
+
 
 fight()
