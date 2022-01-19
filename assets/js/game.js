c42ac573361093104608457110dedbe620d90298
joel1723 <75437050+joel1723@users.noreply.github.com>
2022-01-19 13:30:13 -0500

    math min

--- assets/js/game.js
+++ assets/js/game.js
@@ -48,7 +48,9 @@ var fight = function (enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = Math.max(0, playerHealth - enemyAttack);
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -82,7 +84,11 @@ var startGame = function () {
       var pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = randomNumber();
+      // generate random damage value based on player's attack power
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+
+      enemyHealth = Math.max(0, enemyHealth - damage);
+
 
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
@@ -180,12 +186,12 @@ var shop = function () {
   }
 };
 var randomNumber = function (min, max) {
-  var value = Math.floor(Math.random() * (max = min + 1) + min)
+  var value = Math.floor(Math.random() * (min - max + 1)) + min;
+
+  return value;
+};
 
 
-
-  return value
-}
 
 
 // start first game when page loads
